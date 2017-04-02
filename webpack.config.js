99a6bfe7dbab43ac78f7e97dc492d8e7a2d61ffc
mdahlgrengadd <mdahlgrengadd@gmail.com>
2017-04-02 12:31:05 +0200

    basic functionality working

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,25 @@
     var path = require('path');
     module.exports = {
-        entry: './src/webaudio_myengine.js',
+        devtool: "cheap-module-eval-source-map",
+        resolve: {
+            alias: {
+              'wavesurfer': path.resolve(__dirname, './node_modules/wavesurfer.js/dist/wavesurfer.js')
+            }
+          },
+        module: {
+            rules: [
+                { test: path.join(__dirname, 'example'),
+                  loader: 'babel-loader' }
+            ]
+        },
+        entry: path.join(__dirname, 'src/app.js'),
         output: {
-            path: './src',
-            filename: 'webaudio_bundle.js'
+            path: __dirname,
+            filename: 'app_bundle.js'
         },
         module: {
-            loaders: [
-                { test: path.join(__dirname, 'example'),
+            rules: [
+                { test: path.join(__dirname, 'src'),
                   loader: 'babel-loader' }
             ]
         }
